e8d4a36ae5e1d33c5b63e3f8cde55426c1cb9f86
Filip Kristofiak <filip.kristofiak@gmail.com>
2024-01-19 21:06:33 +0100

    enableCrossJoin global setting

--- src/main/scala/com/filipkr/spark/utils/Environment.scala
+++ src/main/scala/com/filipkr/spark/utils/Environment.scala
@@ -14,13 +14,26 @@ trait Environment {
    *     df1.join(df2,lit(true))
    * }}}
    */
-  //TODO: move to appropriate package
   def enableCrossJoin(implicit ss: SparkSession): Unit = ss.conf.set("spark.sql.crossJoin.enabled", "true")
 
   /**
    * Turns of Spark automatic DataFrame broadcasting in join
    */
   def autoBroadcastOff(implicit ss: SparkSession): Unit = ss.conf.set("spark.sql.autoBroadcastJoinThreshold", -1)
+
+  /**
+   * Allows using untyped Scala UDFs (legacy, using outType, deprecated since 3.0.0)
+   * @example{{{
+   *     // implicit val ss = spark  /* Exists in context */
+   *     allowUntypedUDF             /* Enough to call once */
+   *
+   *     def removeNull: Seq[Row] => Seq[Row] = _.filterNot(_ == null)
+   *     def rowSchema: DataType = ??? // schema of the row
+   *
+   *     df.withColumn("some_col", removeNull.udf(rowSchema)(col("some_col")))
+   * }}}
+   */
+  def allowUntypedUDF(implicit ss: SparkSession) = ss.conf.set("spark.sql.legacy.allowUntypedScalaUDF", "true")
 }
 
 object Environment extends Environment
